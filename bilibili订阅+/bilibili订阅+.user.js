fe082a71f47b4c231b0fe468c03aa724f14c1d86
tyx1703 <tyx1703@gmail.com>
2018-12-24 20:04:44 +0800

    correct subscription's wrong url

--- bilibili订阅+/bilibili订阅+.user.js
+++ bilibili订阅+/bilibili订阅+.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili订阅+
 // @namespace    https://github.com/YanxinTang/Tampermonkey
-// @version      0.4.5
+// @version      0.4.6
 // @description  bilibili导航添加订阅按钮以及订阅列表
 // @author       tyx1703
 // @include      *.bilibili.com/*
@@ -71,7 +71,7 @@
           return this.get_cookie('DedeUserID');
         },
         href(){
-          return `//space.bilibili.com/${this.mid}/#!/bangumi`;
+          return `//space.bilibili.com/${this.mid}/bangumi`;
         }
       },
       methods: {
