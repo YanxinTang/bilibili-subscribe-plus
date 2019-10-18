f96eb2825eceaffa76a19f5f07fa5f52314df456
tyx1703 <tyx1703@gmail.com>
2019-10-19 01:30:24 +0800

    🛠update script

--- bilibili订阅+/bilibili订阅+.user.js
+++ bilibili订阅+/bilibili订阅+.user.js
@@ -1,64 +1,53 @@
 // ==UserScript==
 // @name         bilibili订阅+
 // @namespace    https://github.com/YanxinTang/Tampermonkey
-// @version      0.4.8
+// @version      0.5.0
 // @description  bilibili导航添加订阅按钮以及订阅列表
 // @author       tyx1703
 // @include      *.bilibili.com/*
 // @license      MIT
+// @noframes
+// @require     https://cdn.jsdelivr.net/npm/vue@2/dist/vue.min.js
+// @exclude     *://live.bilibili.com/*
+// @exclude     *://manga.bilibili.com/*
+// @exclude     *://bw.bilibili.com/*
+// @exclude     *://show.bilibili.com/*
 // ==/UserScript==
 
 (function() {
   'use strict';
 
-  let main = function(nav_list){
-    // append subscribe item to nav list
+  const DedeUserID = getCookie('DedeUserID');
+  const loginStatus = DedeUserID !== '';
+  if (!loginStatus) {
+    log("少侠请先登录~  哔哩哔哩 (゜-゜)つロ 干杯~")
+    return;
+  }
 
-    let subscribe_node = document.createElement('li');
-    subscribe_node.setAttribute('id', 'subscribe');
-    subscribe_node.setAttribute('class', 'nav-item');
-    subscribe_node.setAttribute('v-on:mouseover.once', 'onmouseover');
-    subscribe_node.setAttribute('v-on:mouseleave', 'onmouseleave');
-    subscribe_node.innerHTML = `
-      <a id="subscribe-link"
-        class="t"
-        :href="href"
-        @mouseover="onmouseover">订阅</a>
-      <transition name="slide-fade">
-        <div id="subscribe-list"
-          :class="{ 'isActive': show }"
-          v-if="show" 
-          @scroll.stop="onscroll"
-          ref="list">
-          <ul>
-            <li v-for="season in seasons">
-            <a :href="season.share_url" target="_blank">
-              <img :src="season.cover" alt="" class="season-cover" />
-              <span class="season-name">
-                {{ season.title }}
-              </span>
-              <span class="season-tag">
-                {{ formate_tag(season) }}
-              </span>
-            </a>
-            </li>
-          </ul>
-        </div>
-      </transition>
-    `;
-    nav_list.appendChild(subscribe_node);
+  getNavList(main);
+  style();
 
-    let subscribe = new Vue({
-      el: '#subscribe',
+  /**
+   * main
+   * @param {*} navList 
+   */
+  function main(navList) {
+    const subscribeMenuEl = document.createElement('li');
+    subscribeMenuEl.setAttribute('id', 'subscribe');
+    navList.appendChild(subscribeMenuEl);
+    const subscribe = new Vue({
+      el: subscribeMenuEl,
       data: {
         show: false,
         seasons: [],
         loadflag: true,
         pages: -1,             // count of pages
         page: 1,              // current page
+        mid: '',
       },
-      mounted(){
-        this.get_subscribe();
+      created() {
+        this.mid = DedeUserID;
+        this.getSubscribe();
       },
       updated(){
         this.loadflag = true; // allow loading after update data
@@ -67,39 +56,35 @@
         url(){
           return `//space.bilibili.com/ajax/Bangumi/getList?mid=${this.mid}&page=${this.page}`;
         },
-        mid(){
-          return this.get_cookie('DedeUserID');
-        },
         href(){
           return `//space.bilibili.com/${this.mid}/bangumi`;
         }
       },
       methods: {
-        get_cookie(name){
-          const value = "; " + document.cookie;
-          let parts = value.split("; " + name + "=");
-          if (parts.length == 2) {
-            return parts.pop().split(";").shift();
-          }
-          return undefined;
+        getSubscribe(){
+          return fetch(this.url)
+            .then((response) => {
+              if (response.ok) {
+                return response.json()
+              } else {
+                return Promise.reject(new Error(`${response.url}: ${response.status}`))
+              }
+            })
+            .then((data) => {
+              const newSeasons = data.data.result;
+              this.seasons = [...this.seasons, ...newSeasons];
+              if (this.pages <= 0) {
+                const count = data.data.count;
+                this.pages = Math.ceil(count / newSeasons.length);
+              }
+              this.page++;
+              log('Load successfully ^.^')
+            })
+            .catch(error => {
+              log(error)
+            })
         },
-        get_subscribe(){
-          this.$http.get(this.url)
-          .then((response) => {
-            const newSeasons = response.body.data.result;
-            this.seasons = [...this.seasons, ...newSeasons];
-            if (this.pages <= 0) {
-              const count = response.body.data.count;
-              this.pages = Math.ceil(count / newSeasons.length);
-            }
-            this.page++;
-            log('Load successfully ^.^')
-          }, () => {
-            // error callback
-            log('Something was wrong when getting resource.=.=\\\\\\');
-          });
-        },
-        formate_tag(season){
+        formateTag(season){
           let tag='';   //标签内容
           if(season.is_finish === 0){
               if(season.newest_ep_index === -1){
@@ -125,48 +110,87 @@
             && this.$refs.list.scrollHeight - this.$refs.list.scrollTop - 50 <=  this.$refs.list.clientHeight
             ){
             this.loadflag = false;  // refuse to load
-            this.get_subscribe();
+            this.getSubscribe();
           }
         }
-      }
+      },
+      template: `
+        <li 
+          id="subscribe"
+          class="nav-item"
+          @mouseover.once="onmouseover"
+          @mouseleave="onmouseleave"
+        >
+          <a id="subscribe-link"
+            class="t"
+            :href="href"
+            @mouseover="onmouseover">
+            订阅
+          </a>
+          <transition name="slide-fade">
+            <div id="subscribe-list"
+              :class="{ 'isActive': show }"
+              v-if="show" 
+              @scroll.stop="onscroll"
+              ref="list">
+              <ul>
+                <li v-for="season in seasons">
+                <a :href="season.share_url" target="_blank">
+                  <img :src="season.cover" alt="" class="season-cover" />
+                  <span class="season-name">
+                    {{ season.title }}
+                  </span>
+                  <span class="season-tag">
+                    {{ formateTag(season) }}
+                  </span>
+                </a>
+                </li>
+              </ul>
+            </div>
+          </transition>
+        </li>
+      `,
     });
   }
 
   /**
-   * print something in console with custom style
-   * @param {*} stuff 
+   * get nav list on the right of header
+   * @param {Function} main 
    */
-  function log(stuff) {
-    console.log('%cbilibili订阅+:', 'background: #f25d8e; border-radius: 3px; color: #fff; padding: 0 8px', stuff);
-  }
-
-  /**
-   * get the ul node of nav list item. It will be called agter jquery and vue loaded in init_lib
-   * @return {[type]} null
-   */
-  let get_nav_list = function(callback){
-    let nav_list = document.querySelector('div.nav-con.fr>ul.fr');
-    if(nav_list){
-      main(nav_list);
-      log('Get nav menu list');
-    }else{
-      let observer = new MutationObserver(function (mutations, observer) {
-        mutations.forEach(function(mutation) {
-          if(mutation.addedNodes.length>0){
-            const addedNode = mutation.addedNodes[0];
-            const navList = isNavList(addedNode);
-            if(navList){
-              log('Get nav menu list');
-              main(navList);
-              ovserver.disconnect();
-            }          
+  function getNavList (main) {
+    const navList = document.body.querySelector('div.nav-wrapper-right .nav-con .nav-con-ul');
+    if(isNavList(navList)){
+      log('Get nav menu list directly');
+      main(navList);
+    } else {
+      const navListWrapper = document.querySelector('div.nav-wrapper-right .nav-con');
+      if (navListWrapper) {
+        const observer = new MutationObserver((mutations, ovserver) => {
+          for (const mutation of mutations) {
+            if(mutation.addedNodes.length>0){
+              const addedNode = mutation.addedNodes[0];
+              if(isNavList(addedNode)){
+                log('Get nav menu list by observing');
+                main(addedNode);
+                ovserver.disconnect();
+                break;
+              }  
+            }
           }
         });
-      });
-      observer.observe(document.body, {
-        'childList': true,
-        'subtree': true
-      });
+        observer.observe(navListWrapper, {
+          childList: true,
+          subtree: false,
+        });
+      } else {
+        const timer = setInterval(() => {
+          const navList = document.body.querySelector('div.nav-wrapper-right .nav-con .nav-con-ul');
+          if (navList) {
+            main(navList);
+            clearInterval(timer)
+          }
+        }, 300);
+      }
     }
   }
 
@@ -175,48 +199,42 @@
    * @param {*} node 
    */
   function isNavList(node) {
-    const navListTemp = node.querySelector('ul.fr');
-    if(navListTemp){
-      return navListTemp;
-    } else if (node.tagName === 'UL' && node.classList.contains('fr')){
-      return node;
+    if (
+      node
+      && node.tagName
+      && node.tagName.toLowerCase() === 'ul'
+      && node.classList.contains('nav-con-ul')  
+    ) {
+      return true;
     }
     return false;
   }
 
   /**
-   * @param  {Function} callback callback function when jquery and vue is loaded
-   * @return {[type]} null
+   * @returns void
    */
-  function lib_loader(callback) {
-    let head = document.head || document.getElementsByTagName('head')[0];
-    let vue_script_node = document.createElement('script');
-    vue_script_node.setAttribute("src", "//cdn.jsdelivr.net/combine/npm/vue@2.5.17,npm/vue-resource@1.5.1");
-    vue_script_node.addEventListener('load', callback, false);
-    head.appendChild(vue_script_node);
-    log('Vue has been loaded');
-  }
-
-  /**
-   * @return {undifined}
-   */
-  function style(){
+  function style() {
     let head = document.head || document.getElementsByTagName('head')[0];
     let style = document.createElement('style');
+    const listNewVersionOffset = '-200px';
+    const listOldVersionOffset = '-101px';
+    const path = location.pathname;
+    const newVersion = getCookie('stardustpgcv') === '0606' && isPlayerPage(path);
+
     style.textContent = `
       #subscribe-list{
         width: 250px;
         height: 340px;
         overflow-y: auto;
         position: absolute;
-        top: 42px;
-        left: -101px;
+        top: 100%;
+        left: ${newVersion ? listNewVersionOffset : listOldVersionOffset};
         border-radius:  0 0 4px 4px;
         background: #fff;
         box-shadow: rgba(0,0,0,0.16) 0 2px 4px;
         text-align: left;
         font-size: 12px;
-        z-index: 7000;
+        z-index: ${newVersion ? 'unset' : 1};
         transition: all .3s ease-out .25s;
       }
 
@@ -268,10 +286,33 @@
   }
 
   /**
-   * @param  {[string]} name  name of cookie
-   * @return {[string]} string of specified cookie
+   * Get cookie by name
+   * @param {string} name 
    */
+  function getCookie(name){
+    const value = "; " + document.cookie;
+    let parts = value.split("; " + name + "=");
+    if (parts.length == 2) {
+      return parts.pop().split(";").shift();
+    }
+    return '';
+  }
 
-  lib_loader(get_nav_list);
-  style();
+  /**
+   * Test url path is media player page
+   * @param {string} path 
+   * @returns {boolean}
+   */
+  function isPlayerPage(path) {
+    const reg = /.*\/play\/.+/;
+    return reg.test(path)
+  }
+
+  /**
+   * print something in console with custom style
+   * @param {*} stuff 
+   */
+  function log(stuff) {
+    console.log('%cbilibili订阅+:', 'background: #f25d8e; border-radius: 3px; color: #fff; padding: 0 8px', stuff);
+  }
 })();
